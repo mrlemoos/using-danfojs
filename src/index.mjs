ca882ce8b8523636599b7f58f89f0f19a9b567ef
Leonardo Lemos <leonardo@sbsistemas.com.br>
2022-03-23 20:55:46 -0300

    feat: create a project

---
+++ src/index.mjs
@@ -0,0 +1,13 @@
+import { DataFrame } from "danfojs-node";
+
+const data = [
+  { A: 0.4612, B: 4.28283, C: -1.509, D: -1.1352 },
+  { A: 0.5112, B: -0.22863, C: -3.39059, D: 1.1632 },
+  { A: 0.6911, B: -0.82863, C: -1.5059, D: 2.1352 },
+  { A: 0.4692, B: -1.28863, C: 4.5059, D: 4.1632 },
+];
+
+const dataFrame = new DataFrame(data);
+
+dataFrame.print();
+dataFrame.ctypes.print();
